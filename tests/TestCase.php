0f15e705153490ce5658382a3d24db704ec0ba36
Roberto <roby.belotti@gmail.com>
2018-09-12 21:34:37 +0200

    Test classes created

---
+++ tests/TestCase.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright (c) 2017 - present
+ * LaravelGoogleRecaptcha - TestCase.php
+ * author: Roberto Belotti - roby.belotti@gmail.com
+ * web : robertobelotti.com, github.com/biscolab
+ * Initial version created on: 12/9/2018
+ * MIT license: https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
+ */
+
+namespace Biscolab\ReCaptcha\Tests;
+
+use Biscolab\ReCaptcha\Facades\ReCaptcha;
+use Biscolab\ReCaptcha\ReCaptchaServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+/**
+ * Class TestCase
+ * @package Biscolab\ReCaptcha\Tests
+ */
+class TestCase extends OrchestraTestCase {
+
+    /**
+     * Load package alias
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app) {
+
+        return [
+            'ReCaptcha' => ReCaptcha::class,
+        ];
+    }
+
+    /**
+     * Load package service provider
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app) {
+
+        return [ReCaptchaServiceProvider::class];
+    }
+}
