8cdce02a9b40acef109320298487212cac401586
Roberto <roby.belotti@gmail.com>
2018-09-12 21:34:14 +0200

    Code styling

--- src/ReCaptchaBuilderV2.php
+++ src/ReCaptchaBuilderV2.php
@@ -1,25 +1,29 @@
 <?php
-
 /**
- *
- * Biscolab Laravel ReCaptcha - ReCaptchaBuilderV2 Class
- * MIT License @ https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
- * author: Roberto Belotti - info@robertobelotti.com
+ * Copyright (c) 2017 - present
+ * LaravelGoogleRecaptcha - ReCaptchaBuilderV2.php
+ * author: Roberto Belotti - roby.belotti@gmail.com
  * web : robertobelotti.com, github.com/biscolab
- *
+ * Initial version created on: 12/9/2018
+ * MIT license: https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
  */
 
 namespace Biscolab\ReCaptcha;
 
+/**
+ * Class ReCaptchaBuilderV2
+ * @package Biscolab\ReCaptcha
+ */
 class ReCaptchaBuilderV2 extends ReCaptchaBuilder {
 
-	/**
-	 * Write ReCAPTCHA HTML tag in your FORM
-	 * Insert before </form> tag
-	 */
-	public function htmlFormSnippet()
-	{
-		return ($this->version == 'v2')? '<div class="g-recaptcha" data-sitekey="'.$this->api_site_key.'"></div>' : '';
-	}
+    /**
+     * Write ReCAPTCHA HTML tag in your FORM
+     * Insert before </form> tag
+     * @return string
+     */
+    public function htmlFormSnippet() {
+
+        return ($this->version == 'v2') ? '<div class="g-recaptcha" data-sitekey="' . $this->api_site_key . '"></div>' : '';
+    }
 
 }
--- src/ReCaptchaServiceProvider.php
+++ src/ReCaptchaServiceProvider.php
@@ -1,13 +1,24 @@
 <?php
+/**
+ * Copyright (c) 2017 - present
+ * LaravelGoogleRecaptcha - ReCaptchaServiceProvider.php
+ * author: Roberto Belotti - roby.belotti@gmail.com
+ * web : robertobelotti.com, github.com/biscolab
+ * Initial version created on: 12/9/2018
+ * MIT license: https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
+ */
 
 namespace Biscolab\ReCaptcha;
 
 use Illuminate\Support\ServiceProvider;
-use Biscolab\ReCaptcha\ReCaptchaBuilder;
 use Validator;
 
-class ReCaptchaServiceProvider extends ServiceProvider
-{
+/**
+ * Class ReCaptchaServiceProvider
+ * @package Biscolab\ReCaptcha
+ */
+class ReCaptchaServiceProvider extends ServiceProvider {
+
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -15,9 +26,11 @@ class ReCaptchaServiceProvider extends ServiceProvider
      */
     protected $defer = false;
 
+    /**
+     *
+     */
+    public function boot() {
 
-    public function boot()
-    {
         $this->addValidationRule();
         $this->publishes([
             __DIR__ . '/../config/recaptcha.php' => config_path('recaptcha.php'),
@@ -25,12 +38,22 @@ class ReCaptchaServiceProvider extends ServiceProvider
     }
 
     /**
+     * Extends Validator to include a recaptcha type
+     */
+    public function addValidationRule() {
+
+        Validator::extendImplicit('recaptcha', function ($attribute, $value, $parameters, $validator) {
+
+            return app('recaptcha')->validate($value);
+        }, trans('validation.recaptcha'));
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
 
         $this->mergeConfigFrom(
             __DIR__ . '/../config/recaptcha.php', 'recaptcha'
@@ -44,32 +67,26 @@ class ReCaptchaServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function registerReCaptchaBuilder()
-    {
+    protected function registerReCaptchaBuilder() {
+
         $this->app->singleton('recaptcha', function ($app) {
-            if(config('recaptcha.version') == 'v2') return new ReCaptchaBuilderV2(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'), config('recaptcha.version'));
-            else return new ReCaptchaBuilderInvisible(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'), config('recaptcha.version'));
+
+            if (config('recaptcha.version') == 'v2') {
+                return new ReCaptchaBuilderV2(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'), config('recaptcha.version'));
+            } else {
+                return new ReCaptchaBuilderInvisible(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'), config('recaptcha.version'));
+            }
         });
     }
-
-    /**
-     * Extends Validator to include a recaptcha type
-     */
-    public function addValidationRule()
-    {
-        Validator::extendImplicit('recaptcha', function($attribute, $value, $parameters, $validator) {
-            return app('recaptcha')->validate($value);
-        }, trans('validation.recaptcha'));
-    }   
 
     /**
      * Get the services provided by the provider.
      *
      * @return array
      */
-    public function provides()
-    {
+    public function provides() {
+
         return ['recaptcha'];
     }
- 
+
 }
