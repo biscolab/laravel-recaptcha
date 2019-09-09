be31b0e6897aaaf74670fad431ad473ffb08af33
Roberto <roby.belotti@gmail.com>
2019-09-09 23:14:17 +0200

    Provider optimization and "config" group added to publish

--- src/ReCaptchaServiceProvider.php
+++ src/ReCaptchaServiceProvider.php
@@ -11,8 +11,8 @@
 namespace Biscolab\ReCaptcha;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
-use Validator;
 
 /**
  * Class ReCaptchaServiceProvider
@@ -21,109 +21,108 @@ use Validator;
 class ReCaptchaServiceProvider extends ServiceProvider
 {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 *
-	 */
-	public function boot()
-	{
+    /**
+     *
+     */
+    public function boot()
+    {
 
-		$this->addValidationRule();
-//		$this->loadRoutesFrom(__DIR__ . '/routes/routes.php');
-		$this->registerRoutes();
-		$this->publishes([
-			__DIR__ . '/../config/recaptcha.php' => config_path('recaptcha.php'),
-		]);
+        $this->addValidationRule();
+        $this->registerRoutes();
+        $this->publishes([
+            __DIR__ . '/../config/recaptcha.php' => config_path('recaptcha.php'),
+        ], 'config');
 
-	}
+    }
 
-	/**
-	 * Extends Validator to include a recaptcha type
-	 */
-	public function addValidationRule()
-	{
+    /**
+     * Extends Validator to include a recaptcha type
+     */
+    public function addValidationRule()
+    {
 
-		Validator::extendImplicit('recaptcha', function ($attribute, $value, $parameters, $validator) {
+        Validator::extendImplicit(recaptchaRuleName(), function ($attribute, $value) {
 
-			return app('recaptcha')->validate($value);
+            return app('recaptcha')->validate($value);
 		}, trans('validation.recaptcha'));
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
 
-		$this->mergeConfigFrom(
-			__DIR__ . '/../config/recaptcha.php', 'recaptcha'
-		);
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/recaptcha.php', 'recaptcha'
+        );
 
-		$this->registerReCaptchaBuilder();
-	}
+        $this->registerReCaptchaBuilder();
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides(): array
-	{
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
 
-		return ['recaptcha'];
-	}
+        return ['recaptcha'];
+    }
 
-	/**
-	 * @return ReCaptchaServiceProvider
-	 *
-	 * @since v3.4.1
-	 */
-	protected function registerRoutes(): ReCaptchaServiceProvider
-	{
+    /**
+     * @return ReCaptchaServiceProvider
+     *
+     * @since v3.4.1
+     */
+    protected function registerRoutes(): ReCaptchaServiceProvider
+    {
 
-		Route::get(
-			config('recaptcha.default_validation_route', 'biscolab-recaptcha/validate'),
-			['uses' => 'Biscolab\ReCaptcha\Controllers\ReCaptchaController@validateV3']
-		)->middleware('web');
+        Route::get(
+            config('recaptcha.default_validation_route', 'biscolab-recaptcha/validate'),
+            ['uses' => 'Biscolab\ReCaptcha\Controllers\ReCaptchaController@validateV3']
+        )->middleware('web');
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Register the HTML builder instance.
-	 *
-	 * @return void
-	 */
-	protected function registerReCaptchaBuilder()
-	{
+    /**
+     * Register the HTML builder instance.
+     *
+     * @return void
+     */
+    protected function registerReCaptchaBuilder()
+    {
 
-		$this->app->singleton('recaptcha', function ($app) {
+        $this->app->singleton('recaptcha', function ($app) {
 
-			$recaptcha_class = '';
+            $recaptcha_class = '';
 
-			switch (config('recaptcha.version')) {
-				case 'v3' :
-					$recaptcha_class = ReCaptchaBuilderV3::class;
-					break;
-				case 'v2' :
-					$recaptcha_class = ReCaptchaBuilderV2::class;
-					break;
-				case 'invisible':
-					$recaptcha_class = ReCaptchaBuilderInvisible::class;
-					break;
-			}
+            switch (config('recaptcha.version')) {
+                case 'v3' :
+                    $recaptcha_class = ReCaptchaBuilderV3::class;
+                    break;
+                case 'v2' :
+                    $recaptcha_class = ReCaptchaBuilderV2::class;
+                    break;
+                case 'invisible':
+                    $recaptcha_class = ReCaptchaBuilderInvisible::class;
+                    break;
+            }
 
-			return new $recaptcha_class(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'));
+            return new $recaptcha_class(config('recaptcha.api_site_key'), config('recaptcha.api_secret_key'));
 
-		});
-	}
+        });
+    }
 
 }
